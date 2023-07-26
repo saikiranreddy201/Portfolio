261bac4d19011b1cdc13d6970ff9e66a65060d03
saikiranreddy201 <saikiranreddy2021@gmail.com>
2023-07-27 00:54:33 +0530

    Showing Tech in mobile view

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -24,7 +24,14 @@ const Tech = () => {
 
   }, [])
 
-  return (isMobile ? null : 
+  return (isMobile ? 
+    <div className="flex flex-row flex-wrap justify-center gap-10">
+    {technologies.map((technology) => (
+      <div className="w-28 h-28" key={technology.name}>
+        <img src={technology.icon} alt="" />
+      </div>
+    ))}
+  </div> : 
     <>
       <div className="flex flex-row flex-wrap justify-center gap-10">
         {technologies.map((technology) => (
